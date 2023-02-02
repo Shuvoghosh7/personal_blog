29840d106669d16e7ebe645eed4aba270b4544b1
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2023-02-02 19:38:50 +0600

    Show Blog List in ui

--- src/Page/Dashboard/BlogList.js
+++ src/Page/Dashboard/BlogList.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { AiTwotoneDelete } from 'react-icons/ai';
 
 const BlogList = () => {
+    const [blogs, setBlogs] = useState([])
+    useEffect(() => {
+        fetch('http://localhost:5000/api/v1/blog')
+            .then(res => res.json())
+            .then(data => setBlogs(data.data))
+    }, [])
     return (
-        <div>
-            <h1>Blog List</h1>
+        <div class="overflow-x-auto">
+            <table class="table table-zebra w-full">
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Blog Title</th>
+                        <th>Blog Category</th>
+                        <th>Blog  Date</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        blogs?.map((blog, index) =>
+                            <tr>
+                                <th>{index + 1}</th>
+                                <td>{blog.blogTitle}</td>
+                                <td>{blog.blogCategory}</td>
+                                <td>{blog.blogDate}</td>
+                                <td>
+                                    <div>
+                                        <button className='text-xl text-red-600' ><AiTwotoneDelete /></button>
+                                    </div>
+                                </td>
+
+                            </tr>
+
+
+                        )
+                    }
+
+                </tbody>
+            </table>
         </div>
     );
 };
